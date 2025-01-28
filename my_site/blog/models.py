8e160557a4fcdbbb2bc4380348df8d38c0c78e0b
kevboz <64555058+kevboz@users.noreply.github.com>
2025-01-27 20:46:42 -0500

    added to SQL and model views

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import MinLengthValidator ,MaxLengthValidator
+
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+    
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+    def __str__(self):
+        return self.full_name()
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.caption
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=250)
+    image_name = models.CharField(max_length=100)
+    post_date = models.DateField(auto_now=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    slug = models.SlugField(unique=True, db_index=True) # automatically does index.. don't really need db_index
+    context = models.TextField(validators=[MinLengthValidator(10), MaxLengthValidator(5000)])
+    tag = models.ManyToManyField(Tag)
+
+
+
+    
